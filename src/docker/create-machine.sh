3788ddecffc933d7dff062ed2c0fea135cd5d31a
Holger Stolzenberg <h.stolzenberg@ewerk.com>
2020-03-22 12:21:44 +0100

    Use defined network address range for docker-machine to avoid VPN clashes

--- src/docker/create-machine.sh
+++ src/docker/create-machine.sh
@@ -6,6 +6,7 @@ MEMORY_MB="2048"
 
 docker-machine create \
   --driver virtualbox \
+  --virtualbox-hostonly-cidr "192.168.56.1/24" \
   --virtualbox-cpu-count ${CPU_COUNT} \
   --virtualbox-memory ${MEMORY_MB} \
   --virtualbox-ui-type "headless" ${NAME} || echo "Machine created."
